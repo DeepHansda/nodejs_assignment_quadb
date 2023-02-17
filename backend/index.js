a28aef5b645603230b8f877a00c60c1854f68fa5
Deep Hansda <deephansda921@gmail.com>
2023-02-17 23:49:32 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const connection = require("./src/db/connection");
-const callAPI  = require("./src/utils/parser");
+const callAPI = require("./src/utils/parser");
 const app = express();
 const PORT = process.env.PORT || 3400;
 
-
 // **** initializing database
-connection()
+connection();
 // **** initializing database
 
-setInterval(()=>callAPI(),60000)
-
-
-
-
+// ***** storing data in database from api
+setInterval(() => callAPI(), 60000);
 
 app.get("/", (req, res) => {
   res.status(200).json({
